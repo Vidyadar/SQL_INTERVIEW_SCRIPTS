7b645719313b3fc28a80b201a49336b551bfc6e3
vidhyadar <99064524+Vidyadar@users.noreply.github.com>
2025-08-07 07:56:31 +0530

    Add files via upload

---
+++ Employee Table For salary.sql
@@ -0,0 +1,24 @@
+CREATE TABLE emp (
+  emp_name VARCHAR(50),
+  emp_salary DECIMAL(10,2)
+);
+
+INSERT INTO emp (emp_name, emp_salary) VALUES
+('Shubham Thakur', 50000.00),
+('Aman Chopra', 60000.50),
+('Naveen Tulasi', 75000.75),
+('Bhavika uppala', 45000.25),
+('Nishant jain', 80000.00);
+
+
+CREATE TABLE department(
+    ID int,
+    SALARY int,
+    NAME Varchar(20),
+    DEPT_ID Varchar(255));
+
+	INSERT INTO department VALUES (1, 34000, 'ANURAG', 'UI DEVELOPERS');
+INSERT INTO department VALUES (2, 33000, 'harsh', 'BACKEND DEVELOPERS');
+INSERT INTO department VALUES (3, 36000, 'SUMIT', 'BACKEND DEVELOPERS');
+INSERT INTO department VALUES (4, 36000, 'RUHI', 'UI DEVELOPERS');
+INSERT INTO department VALUES (5, 37000, 'KAE', 'UI DEVELOPERS');
