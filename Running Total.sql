c6608b72bd98df721b5ee833d99c2661b210ebc0
vidhyadar <99064524+Vidyadar@users.noreply.github.com>
2025-08-15 01:24:21 +0530

    Add files via upload

--- Running Total.sql
+++ Running Total.sql
@@ -3,3 +3,10 @@ select *,
 sum(mark)over(order by personid) as Running_total
 from persons
 order by PersonID
+/*
+Table values
+1	Ram	250.000000
+2	Ramesh	350.000000
+3	Sujit	150.000000
+4	Suresh	400.000000
+NULL	NULL	NULL  */
